d1dcf2f785da7e11a165b69091ce8bfde4dc0952
AnnieKia <annabelle.roche20@my.stjohns.edu>
2022-11-02 12:08:35 -0400

    Initial commit

---
+++ quizjs.js
@@ -0,0 +1,204 @@
+const quiz1 = [
+  {
+    questionType: "true_false",
+    questionText: "RAM stands for 'Random Access Memory'",
+    correctAnswer: "True",
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  }
+]
+const quiz2 = [
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  },
+  {
+    questionType: ,
+    questionText: ,
+    correctAnswer: ,
+  }
+]

f0ad5f6797d132c5cd1b2952fd25e4cb56ec856b
AnnieKia <annabelle.roche20@my.stjohns.edu>
2022-11-03 15:17:16 -0400

    pain

--- quizapp.js
+++ quizapp.js
@@ -1,12 +1,15 @@
 const appState = {
-  current_view : "#intro-view",
+  current_view : "#intro_view",
   current_quiz : "none",
   current_question: -1,
-  current_model: {}
+  current_model: {
+    action: "quiz",
+    answer: "null"
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  appState.current_view = "#intro-view";
+  appState.current_view = "#intro_view";
   appState.current_model = {
     action: "quiz"
   }
@@ -22,9 +25,8 @@ function handle_widget_event(e){
 
       appState.current_quiz= e.target.dataset.action
       appState.current_question = 0
-      appState.current_model = loadModel(appState.current_quiz, appState.current_question);
+      loadModel(appState.current_quiz, appState.current_question);
       setQuestionView(appState);
-
       update_view(appState);
 
   }
@@ -34,21 +36,67 @@ function handle_widget_event(e){
       isCorrect = check_user_response(e.target.dataset.answer, appState.current_model);
 
       app.current_question += 1;
-      appState.current_model = loadModel()
+      loadModel(appState.current_quiz, appState.current_question);
     }
   }
-  if(appState.current_view == "#question_view_true_false")
+  if(appState.current_view == "#question_view_true_false"){
+    if (e.target.dataset.action == "answer") {
+       // Controller - implement logic.
+       isCorrect = check_user_response(e.target.dataset.answer, appState.current_model);
+
+       // Update the state.
+       appState.current_question =   appState.current_question + 1;
+       loadModel(appState.current_quiz, appState.current_question);
+       setQuestionView(appState);
+       // Update the view.
+       update_view(appState);
+
+     }
+
+  }
 }
 
-function loadModel(quiz, question){
-  const response = await fetch(`https://my-json-server.typicode.com/anniekia/quizapp/${quiz}/${current_question}`);
-    const question = await response.json();
+async function loadModel(quiz, question){
+  await fetch (`https://my-json-server.typicode.com/anniekia/quizapp/${quiz}/${question}`)
+    .then(response => response.json())
+    .then(data => {
+      appState.current_model = data;
+    })
 
-    return questionText;
 }
-check_user_response(input, answer){
+
+function check_user_response(input, answer){
   if(input == answer){
     return true;
   }
   return false;
+}
+
+function update_view(appState){
+  const html_element = render_widget(appState.current_model, appState.current_view);
+  document.querySelector("#widget_view").innerHTML = html_element;
+}
+
+const render_widget = (model, view) => {
+  template_source = document.querySelector(view).innerHTML;
+
+  var template = Handlebars.compile(template_source)
+
+  var html_widget_element = template({...model,...appState})
+  return html_widget_element
+}
+
+async function setQuestionView(appState) {
+  if (appState.current_question == -2){
+    appState.current_view = "#end_view";
+    return
+  }
+  fetch (`https://my-json-server.typicode.com/anniekia/quizapp/${appState.current_quiz}/${appState.current_question}`)
+    .then(response => response.json())
+    .then(data => {
+      appState.current_view = `#question_view_${data.questionType}`
+      console.log(appState);
+    })
+
+
 }
